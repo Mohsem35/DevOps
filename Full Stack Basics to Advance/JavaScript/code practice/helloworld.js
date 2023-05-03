8e9faacdeb800032e1ebf8da14098522f25f8cda
Mohsem35 <tuhin16101996@gmail.com>
2023-05-04 01:02:41 +0600

    nodejs installed

---
+++ Full Stack Basics to Advance/JavaScript/code practice/helloworld.js
@@ -0,0 +1,13 @@
+//console.log("Hello World");
+
+let age = 23;
+
+let result = age >= 18 ? "adult" : "not eligible";
+console.log(result);
+
+
+// ternary operator to check the eligibility to vote
+// let age = 15;
+// let result =
+//     (age >= 18) ? "You are eligible to vote." : "You are not eligible to vote yet";
+// console.log(result);
